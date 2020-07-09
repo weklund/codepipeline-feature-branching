6418d9b12885e31c147cf27f89aa6a6bd1462ca0
Westley Eklund <s.wes35@gmail.com>
2020-07-08 22:38:53 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,54 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    state = {
+        count: 0,
+    }
+
+    increment = () => {
+        this.setState({
+            count: this.state.count + 1,
+        })
+    }
+
+    decrement = () => {
+        this.setState({
+            count: this.state.count - 1,
+        })
+    }
+
+    render() {
+        return(
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+
+                    <p>Here is a fancy counter: {this.state.count}</p>
+
+                    <button onClick={this.increment}>
+                        Increment
+                    </button>
+
+                    <button onClick={this.decrement}>
+                        Decrement
+                    </button>
+
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        )
+    }
 }
 
 export default App;
